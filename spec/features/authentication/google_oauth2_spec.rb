f4e091544e7f7af4c500b3d08f66c8be77a329c9
Mindo-Joseph <mindo.joseph.mj@gmail.com>
2023-04-19 12:53:08 +0300

    configure mock google auth request

--- spec/features/authentication/google_oauth2_spec.rb
+++ spec/features/authentication/google_oauth2_spec.rb
@@ -1,6 +1,21 @@
 RSpec.feature "Social Authentication", type: :feature do
 
-  scenario "User signs in with Google do" do
+  def mock_google_auth
+    OmniAuth.config.mock_auth[:google_oauth2] = OmniAuth::AuthHash.new({
+      provider: "google_oauth2",
+      uid: ENV["fake_google_uid"],
+      info: {
+        name: "John Doe",
+        email: "test@apptitude.com",
+      },
+      credentials: {
+        token: ENV["fake_google_token"]
+      }
+    })
+  end
+
+  scenario "User signs up with Google do" do
+    mock_google_auth
     visit new_user_registration_path
     click_link "Sign in with Google"
     expect(page).to have_content("Successfully authenticated from Google account.")
--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -31,6 +31,15 @@ rescue ActiveRecord::PendingMigrationError => e
   abort e.to_s.strip
 end
 RSpec.configure do |config|
+
+  config.before(:each) do
+    OmniAuth.config.test_mode = true  
+  end
+
+  config.after(:each) do
+    OmniAuth.config.test_mode = false
+
+  end
   # Remove this line if you're not using ActiveRecord or ActiveRecord fixtures
   config.fixture_path = "#{::Rails.root}/spec/fixtures"
 
