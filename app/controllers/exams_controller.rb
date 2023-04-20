7781da0a7b3b0c6e9f8989933989148974ec8973
Mindo-Joseph <mindo.joseph.mj@gmail.com>
2023-04-20 11:32:07 +0300

    define exam model

---
+++ app/controllers/exams_controller.rb
@@ -0,0 +1,53 @@
+class ExamsController < ApplicationController
+  skip_before_action :verify_authenticity_token
+
+  def index
+    @exams = Exam.all
+    # if no exams return json message of no exams
+    if @exams.empty?
+      render json: {message: "No exams found"}
+    else
+      render json: @exams
+    end
+  end
+
+  def show
+    @exam = Exam.find(params[:id])
+    render json: @exam
+  end
+
+  def create
+    @exam = Exam.create(exam_params)
+    if @exam.save
+      # return a json message of exam created
+      render json: {message: "Exam created"}
+    else
+      render json: @exam.errors
+    end
+  end
+
+  def update
+    @exam = Exam.find(params[:id])
+    if @exam.update(exam_params)
+      render json: @exam
+    else
+      render json: @exam.errors
+    end
+  end
+
+  def destroy
+    @exam = Exam.find(params[:id])
+    if @exam.nil?
+      render json: {message: "Exam not found"}
+    end
+    @exam.destroy
+    render json: {message: "Exam deleted"}
+  end
+
+
+
+  private
+  def exam_params
+    params.permit(:title, :description, :user_id, :public, :price, :pass_percentage, :time_limit)
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_04_20_072331) do
+ActiveRecord::Schema[7.0].define(version: 2023_04_20_080035) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -50,6 +50,19 @@ ActiveRecord::Schema[7.0].define(version: 2023_04_20_072331) do
     t.bigint "blob_id", null: false
     t.string "variation_digest", null: false
     t.index ["blob_id", "variation_digest"], name: "index_active_storage_variant_records_uniqueness", unique: true
+  end
+
+  create_table "exams", force: :cascade do |t|
+    t.string "title"
+    t.string "description"
+    t.bigint "user_id", null: false
+    t.boolean "public"
+    t.float "price"
+    t.float "pass_percentage"
+    t.integer "time_limit"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_exams_on_user_id"
   end
 
   create_table "friendly_id_slugs", force: :cascade do |t|
@@ -94,5 +107,6 @@ ActiveRecord::Schema[7.0].define(version: 2023_04_20_072331) do
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
   add_foreign_key "active_storage_variant_records", "active_storage_blobs", column: "blob_id"
+  add_foreign_key "exams", "users"
   add_foreign_key "profiles", "users"
 end
